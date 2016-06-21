d335df6b51555b66ac6ab0715c4a1bf3c086017f
hubao <cmwl98@hotmail.com>
2016-06-21 11:20:41 +0800

    test

---
+++ CommonBaseDefine.h
@@ -0,0 +1,17 @@
+#ifndef _COMMON_BASE_DEFINE_H_
+#define _COMMON_BASE_DEFINE_H_   
+
+ 
+//给外壳程序启动使用的名字调用
+#define X86Shell	"X86Shell.exe"
+#define X86MeetingUI "X1Box_x86.exe"
+#define X86M1AUTOSRV "X86AutoUpdateService.exe"
+//快捷方式创建相关
+
+
+//自动更新引导程序使用的，用来更新后重启使用
+#define PRODUCT_NAME "X86JM1"
+#define SHORTCUT_NAME "X86Shell.exe.lnk"
+
+
+#endif
